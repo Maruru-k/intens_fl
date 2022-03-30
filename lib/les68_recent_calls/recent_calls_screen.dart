ee76798226e0ab7302c8469d5e76cd73d743f0ce
Maruru-k <gasbulla050@gmail.com>
2022-03-30 20:22:00 +0300

    RecentCalls

---
+++ lib/les68_recent_calls/recent_calls_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intens_fl/les68_recent_calls/call_card.dart';
+
+class RecentCalls extends StatelessWidget {
+  const RecentCalls({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Журнал звонков"),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        physics: const BouncingScrollPhysics(),
+        itemCount: 15,
+        itemBuilder: (context, _) => const CallCard(),
+        separatorBuilder: (context, _) => const Padding(
+          padding: EdgeInsets.only(left: 55),
+          child: Divider(thickness: 1),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:intens_fl/layout_task/data_project/navigation_data.dart';
 import 'package:intens_fl/les54.dart';
 import 'package:intens_fl/les63_stful/scroll.dart';
 import 'package:intens_fl/les63_stful/stful.dart';
+import 'package:intens_fl/les68_recent_calls/recent_calls_screen.dart';
 import 'package:intens_fl/my_stack.dart';
 import 'package:intens_fl/screens/intens_task/noble_gas/noble_gas_screen.dart';
 import 'package:intens_fl/screens/intens_task/olimpics/olympics22.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       // theme: ThemeData(colorScheme: const ColorScheme.dark(primary: Color(0xff1f2d3b))),
       debugShowCheckedModeBanner: false,
-      home: ScrollButtons(),
+      home: RecentCalls(),
       title: "Hell o",
     );
   }
